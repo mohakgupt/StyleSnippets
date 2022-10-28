0b81ce148e7ed72c38b406d81f466d2f84566351
mohakgupt <thisismohak@gmail.com>
2022-10-29 01:23:37 +0530

    add buttons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 // import your components and their codes here
-import {Btn1} from './components/buttons'
-import { Btn1Code } from './components/buttons-codes';
+import {Btn1, Btn2, Btn3, Btn4} from './components/buttons'
+import { Btn1Code, Btn2Code, Btn3Code, Btn4Code } from './components/buttons-codes';
 
 import './App.css';
 
@@ -21,6 +21,24 @@ function App() {
         position="right center">
           <pre>
             {Btn1Code}
+          </pre>
+        </Popup>
+        <Popup trigger={<Btn2>Button 2</Btn2>} 
+        position="right center">
+          <pre>
+            {Btn2Code}
+          </pre>
+        </Popup>
+        <Popup trigger={<Btn3>Button 3</Btn3>} 
+        position="right center">
+          <pre>
+            {Btn3Code}
+          </pre>
+        </Popup>
+        <Popup trigger={<Btn4>Button 4</Btn4>} 
+        position="right center">
+          <pre>
+            {Btn4Code}
           </pre>
         </Popup>
         
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -15,3 +15,69 @@ export const Btn1 = styled.button`
         color: #000;
     }
 `
+
+export const Btn2 = styled.button`
+    width: 200px;
+    height: 55px;
+    font-size: 16px;
+    transition: all 200ms;
+    cursor: pointer;
+    display: block;
+    margin: 25px;
+    border: 2px solid orange;
+    color: orange;
+    background-color: rgb(0,0,0,0);
+    &:hover{
+        color: white;
+        border-color: yellow;
+        transform: translateY(-3px);
+        box-shadow: 0px 5px 8px -4px yellow;
+    }
+`
+
+export const Btn3 = styled.button`
+width: 200px;
+  height: 55px;
+  font-size: 16px;
+  transition: all 200ms;
+  cursor: pointer;
+  display: block;
+  margin: 25px;
+  font-weight: 600;
+  border: 5px solid green;
+  border-radius: 10px;
+  color: white;
+  background-color: rgb(0,0,0,0);
+  &:hover{
+    color: greenyellow;
+    border-color: greenyellow;
+    transform: translateY(-3px);
+    box-shadow: 0px 5px 8px -4px yellow;
+  }
+`
+
+export const Btn4 = styled.button`
+width: 200px;
+height: 55px;
+font-size: 16px;
+transition: all 200ms;
+cursor: pointer;
+display: block;
+margin: 25px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    margin: 20px;
+    text-align:center;
+    border: none;
+    background-size: 300% 100%;
+    border-radius: 50px;
+    transition: all .4s ease-in-out;
+    background-image: linear-gradient(to right, #f5ce62, #e43603, #fa7199, #e85a19);
+    box-shadow: 0 4px 15px 0 rgba(229, 66, 10, 0.75);
+    &:hover{
+        background-position: 100% 0;
+        transition: all .4s ease-in-out;
+    }
+`
